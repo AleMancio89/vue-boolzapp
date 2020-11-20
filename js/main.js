35fa960f34e70f9947baf17bdfd95110bc78f85c
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-20 15:57:58 +0100

    Column menu first layout

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+  el : "#root",
+  data: {
+    contacts: [
+      {
+        name: "Michele",
+        imgSrc: "img/avatar_1.jpg"
+      },
+      {
+        name: "Fabio",
+        imgSrc: "img/avatar_2.jpg"
+      },
+      {
+        name: "Samuele",
+        imgSrc: "img/avatar_3.jpg"
+      },
+      {
+        name: "Luisa",
+        imgSrc: "img/avatar_8.jpg"
+      },
+    ]
+  }
+})
