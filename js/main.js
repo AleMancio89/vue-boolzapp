1fcb5147d51c231691b8d2ca1f204792abc4e0bc
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-20 21:49:46 +0100

    First general html done

--- js/main.js
+++ js/main.js
@@ -4,20 +4,34 @@ const app = new Vue({
     contacts: [
       {
         name: "Michele",
-        imgSrc: "img/avatar_1.jpg"
+        imgSrc: "img/avatar_1.jpg",
+        isActive: false,
+        lastSeen:''
       },
       {
         name: "Fabio",
-        imgSrc: "img/avatar_2.jpg"
+        imgSrc: "img/avatar_2.jpg",
+        isActive: false,
+        lastSeen:''
       },
       {
         name: "Samuele",
-        imgSrc: "img/avatar_3.jpg"
+        imgSrc: "img/avatar_3.jpg",
+        isActive: false,
+        lastSeen:''
       },
       {
         name: "Luisa",
-        imgSrc: "img/avatar_8.jpg"
+        imgSrc: "img/avatar_8.jpg",
+        isActive: false,
+        lastSeen:''
       },
     ]
+  },
+  methods: {
+    active: function(actIndex) {
+      this.contacts[actIndex].isActive = !(this.contacts[actIndex].isActive)
+    }
   }
-})
+
+});
