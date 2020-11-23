cb52e023e56f73c26c95e7e7934c21ceb2745457
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-23 12:55:00 +0100

    Html done

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 const app = new Vue({
   el : "#root",
   data: {
+    loggedUser: {
+      name: "Alessandro Mancineli",
+      imgSrc: "img/avatar_io.jpg"
+    },
     contacts: [
       {
         name: "Michele",
         imgSrc: "img/avatar_1.jpg",
-        isActive: false,
-        lastSeen:''
+        lastSeen:'13:23'
       },
       {
         name: "Fabio",
         imgSrc: "img/avatar_2.jpg",
-        isActive: false,
-        lastSeen:''
+        lastSeen:'16:46'
       },
       {
         name: "Samuele",
         imgSrc: "img/avatar_3.jpg",
-        isActive: false,
-        lastSeen:''
+        lastSeen:'8:40'
       },
       {
         name: "Luisa",
         imgSrc: "img/avatar_8.jpg",
-        isActive: false,
-        lastSeen:''
+        lastSeen:'15:15'
       },
-    ]
+    ],
+    activeContactIndex: 0
   },
   methods: {
     active: function(actIndex) {
-      this.contacts[actIndex].isActive = !(this.contacts[actIndex].isActive)
+      this.activeContactIndex = [actIndex];
     }
   }
-
 });
